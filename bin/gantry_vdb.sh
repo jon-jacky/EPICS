a2aff6110a35182855cc4493f8d976b42f3b3843
Jon Jacky <jon@u.washington.edu>
2017-08-15 16:19:28 -0700

    work on gantry_180_err figure for icalepcs conference paper also have to tweak bin/vdb.py protections and bin/gantry_vdb.sh paths etc.

--- bin/gantry_vdb.sh
+++ bin/gantry_vdb.sh
@@ -1,9 +1,15 @@
 
 # this contains vdb.py
-export PATH=$PATH:/users/jon/epics/local/bin
+export PATH=$PATH:/Users/jon/epics/bin
 
 # this contains epicsUtils.py
-export PYTHONPATH=$PYTHONPATH:/users/jon/epics/bessy
+export PYTHONPATH=$PYTHONPATH:/Users/jon/bessy.silver
 
 vdb.py gantry_couch_converters.gantry_180_err.vdb > gantry_180_err.dot
-dotsvg gantry_180_err
+#dotsvg gantry_180_err
+#dotpdf gantry_180_err
+
+vdb.py gantry_couch_converters.gantry_180_err.no_vscan.vdb > gantry_180_err.no_vscan.dot
+#dotsvg gantry_180_err.no_vscan
+#dotpdf gantry_180_err.no_vscan
+
